122b54af34d4e3e60c1ef19ab26e0f7f7217c34a
devltt404 <devltt404@usf.edu>
2024-09-28 20:46:16 -0400

    refactor: update API base URL to use environment variable

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:4000/api/v1" }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_BASE_URL }),
   endpoints: () => ({}),
 });
